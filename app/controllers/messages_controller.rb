55b26d13ae27316ec5de52deff7eb569872ca704
Alexander Kern <alexander.t.kern@gmail.com>
2023-09-19 09:25:27 -0700

    adding messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,22 @@
 class MessagesController < ApplicationController
     def index
         @messages = Message.order(:user)
+        @message = Message.new
     end
+
+
+    def create
+        @message = Message.new(message_params);
+        if @message.save
+            redirect_to messages_path, notice: "Message successful"
+        end
+    end
+
+    private
+
+    def message_params 
+        params.require(:message).permit(:user, :body)
+    end
+
 
 end
